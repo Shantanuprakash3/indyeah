358718f28d2237e3c0c77c654ebac25aa91bd201
Shantanuprakash3 <shantanuyprakash@gmail.com>
2015-09-22 21:17:39 +0530

    added and setup act_as_votable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,7 +62,18 @@ class PostsController < ApplicationController
     end
   end
 
-  private
+  def upvote
+   @post = Post.find(params[:id])
+   @post.upvote_by current_user
+   redirect_to :back
+  end
+  def downvote
+  @post = Post.find(params[:id])
+  @post.downvote_from current_user
+  redirect_to :back
+  end
+  
+private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
